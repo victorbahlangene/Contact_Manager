e4e70992c233d7a90b6f785cc33d845c71a5f9a6
Victor Bahlangene <victorBahlangene96@gmail.com>
2018-11-04 22:30:21 +0300

    Delete using Context API

---
+++ src/components/Contacts.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+import { Consumer } from "../context";
+
+class Contacts extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+
+          return (
+            <React.Fragment>
+              {contacts.map((contactValue /*value */) => (
+                <Contact key={contactValue.id} contact={contactValue} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Contacts;
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+// functional component *rfc*//
+import React from "react";
+//* impt *///
+import PropTypes from "prop-types";
+
+const Header = props => {
+  const { branding } = props;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+//default props
+Header.defaultProps = {
+  branding: "My App"
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+};
+
+//a method of styling
+// const headingStyle = {
+//   color: "green",
+//   fontSize: "50px"
+// };
+
+export default Header;
